4d77a080cc736fb8c4d24a056be2c43d5461479b
Garima <garima89077@gmail.com>
2022-07-02 23:33:43 +0530

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) //we use it to save the instance state of an activity
+    //to transfer the data from one screen to another we need bundles
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
