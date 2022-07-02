aa66e3b2b00794b903ea36776d373fd89719a32c
Garima <garima89077@gmail.com>
2022-07-03 00:09:02 +0530

    callbacks created

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,37 @@ class MainActivity : AppCompatActivity() {
     //to transfer the data from one screen to another we need bundles
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // R is an automated file generated in android
+        println("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called")
     }
 }
