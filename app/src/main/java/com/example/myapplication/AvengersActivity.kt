c736a7038c0c2e9794ae86d6385ee4126b3bff44
Garima <garima89077@gmail.com>
2022-07-07 00:54:39 +0530

    sharedPreferences

--- app/src/main/java/com/example/myapplication/AvengersActivity.kt
+++ app/src/main/java/com/example/myapplication/AvengersActivity.kt
@@ -3,14 +3,14 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AvengersActivity : AppCompatActivity() {
     var titleName:String?="Avengers"
     override fun onCreate(savedInstanceState: Bundle?) //we use it to save the instance state of an activity
     //to transfer the data from one screen to another we need bundles
     {
         super.onCreate(savedInstanceState)
         //activity main to provide UI of app
-        setContentView(R.layout.activity_scrollview) // R is an automated file generated in android
+        setContentView(R.layout.activity_avengers) // R is an automated file generated in android
         //println("onCreate called")
         if(intent!=null){
         titleName=intent.getStringExtra("Name")}
--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,15 +1,16 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity3 : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     lateinit var Phone :EditText
     lateinit var Pswd :EditText
@@ -18,9 +19,22 @@ class MainActivity3 : AppCompatActivity() {
     lateinit var  Register: TextView
     val validMobileNumber="1110001110"
     val validPassword= arrayOf("tony","steve","hulk","thanos")
+    lateinit var sharedPreferences:SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        sharedPreferences=getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        val isLoggedIn=sharedPreferences.getBoolean("isLoggedIn",false)
+        if(isLoggedIn)
+        {
+            val intent= Intent(this@LoginActivity,AvengersActivity::class.java)
+            startActivity(intent)
+        }
+        else
+        {
+            setContentView(R.layout.activity_main2)
+        }
+
         title="Login"
 
         Phone=findViewById(R.id.Phone)
@@ -34,28 +48,35 @@ class MainActivity3 : AppCompatActivity() {
             val mobileNumber=Phone.text.toString()
             val password=Pswd.text.toString()
             var nameOfAvenger="Avenger"
-            val intent= Intent(this@MainActivity3,MainActivity::class.java)
+            val intent= Intent(this@LoginActivity,AvengersActivity::class.java)
             if((mobileNumber==validMobileNumber)){
+                //using when syntax like
+                    //when(password)
+                        //validpassword[0]-> {condiiton}
                 if(password==validPassword[0])
                 {
+                    savePreferences()
                     nameOfAvenger="Iron Man"
                     intent.putExtra("Name",nameOfAvenger)
                     startActivity(intent)
                 }
                 else if(password==validPassword[1])
                 {
+                    savePreferences()
                     nameOfAvenger="Captain America"
                     intent.putExtra("Name",nameOfAvenger)
                     startActivity(intent)
                 }
                 else if(password==validPassword[2])
                 {
+                    savePreferences()
                     nameOfAvenger="The Hulk"
                     intent.putExtra("Name",nameOfAvenger)
                     startActivity(intent)
                 }
                 else if(password==validPassword[3])
                 {
+                    savePreferences()
                     nameOfAvenger="The Avengers"
                     intent.putExtra("Name",nameOfAvenger)
                     startActivity(intent)
@@ -64,7 +85,7 @@ class MainActivity3 : AppCompatActivity() {
             }
             else{
                 Toast.makeText(
-                    this@MainActivity3,
+                    this@LoginActivity,
                     "Incorrect Credentials",
                     Toast.LENGTH_LONG
                 ).show()  //this is known as lambda reprsentation
@@ -77,6 +98,10 @@ class MainActivity3 : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         finish()
+    }
+    fun savePreferences()
+    {
+        sharedPreferences.edit().putBoolean("isLoggedIn",true).apply()
     }
 
 }
