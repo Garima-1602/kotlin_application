2869bfdf41c0af98b6ae30f7de17352d163f5b3b
Garima <garima89077@gmail.com>
2022-07-06 18:51:05 +0530

    adding click to view

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -2,12 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity3 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() , View.OnClickListener {
     //to widen the scope we have declare them before the override
+    override fun onClick(p0: View?) {
+     Toast.makeText(this@MainActivity3,"We clicked on the button to see toast",
+         Toast.LENGTH_LONG).show()
+    }
     lateinit var Phone :EditText
     lateinit var Pswd :EditText
     lateinit var btnClick : Button
@@ -17,5 +23,15 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         title="Login"
+
+        Phone=findViewById(R.id.Phone)
+        Pswd=findViewById(R.id.Pswd)
+        btnClick=findViewById(R.id.btnClick)
+        forgotPswd=findViewById(R.id.forgotPswd)
+        Register=findViewById(R.id.Register)
+
+        btnClick.setOnClickListener(this)
     }
+
+
 }
