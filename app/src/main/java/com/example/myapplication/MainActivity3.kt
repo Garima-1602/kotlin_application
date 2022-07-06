8e448993900e3a78eaffb71fc98364843213e950
Garima <garima89077@gmail.com>
2022-07-06 18:10:52 +0530

    adding variable to main.kt

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -2,8 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
+    //to widen the scope we have declare them before the override
+    lateinit var Phone :EditText
+    lateinit var Pswd :EditText
+    lateinit var btnClick : Button
+    lateinit var  forgotPswd: TextView
+    lateinit var  Register: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
